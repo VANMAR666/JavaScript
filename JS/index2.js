880e83d4fd5f040d9782fa76af8f56d0b7e9e792
semyonkungurov <s.kungurov8679@mail.ru>
2022-12-14 14:12:51 +0700

    combined button functions

--- JS/index2.js
+++ JS/index2.js
@@ -3,40 +3,37 @@ var buttonMinus = document.getElementById('buttonMinus');
 var buttonMultiply = document.getElementById('buttonMultiply');
 var buttonDivide = document.getElementById('buttonDivide');
 
-var input1=document.getElementById('number 1');
-var input2=document.getElementById('number 2');
+var input1 = document.getElementById('number 1');
+var input2 = document.getElementById('number 2');
 
-function getNumber1(){
+function getNumber1() {
     return Number(input1.value);
 }
-function getNumber2(){
+
+function getNumber2() {
     return Number(input2.value);
 }
-function makeOperation(operationCode){
-    if (operationCode==='+') {
-        return getNumber1()+getNumber2();
-    } else if (operationCode==='-'){
-        return getNumber1()-getNumber2();
-    } else if (operationCode==='*'){
-        return getNumber1()*getNumber2();
-    } else if (operationCode==='/') {
-        return getNumber1()/getNumber2();
+
+function makeOperation(operationCode) {
+    if (operationCode === '+') {
+        var result = getNumber1() + getNumber2();
+    } else if (operationCode === '-') {
+        var result = getNumber1() - getNumber2();
+    } else if (operationCode === '*') {
+        var result = getNumber1() * getNumber2();
+    } else if (operationCode === '/') {
+        var result = getNumber1() / getNumber2();
     }
-}
-function onButtonPlusClick(){
-    window.alert(makeOperation('+'));
-}
-function onButtonMinusClick(){
-    window.alert(makeOperation('-'));
-}
-function onButtonMultiplyClick(){
-    window.alert(makeOperation('*'));
-}
-function onButtonDivideClick(){
-    window.alert(makeOperation('/'));
+    window.alert(result);
 }
 
-buttonPlus.addEventListener('click', onButtonPlusClick);
-buttonMinus.addEventListener('click', onButtonMinusClick);
-buttonMultiply.addEventListener('click',onButtonMultiplyClick);
-buttonDivide.addEventListener('click',onButtonDivideClick);
+function onOperationButtonClick(eventObject) {
+    var clickedElement = eventObject.currentTarget;
+    var operation = clickedElement.innerHTML;
+    makeOperation(operation);
+}
+
+buttonPlus.addEventListener('click', onOperationButtonClick);
+buttonMinus.addEventListener('click', onOperationButtonClick);
+buttonMultiply.addEventListener('click', onOperationButtonClick);
+buttonDivide.addEventListener('click', onOperationButtonClick);
